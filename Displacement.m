7615b76381988c0057aadeeb124323977b85ece7
Andrew <71098842+AuxiliaryPanther@users.noreply.github.com>
2020-10-24 01:34:57 +0100

    Add files via upload

---
+++ Displacement.m
@@ -0,0 +1,21 @@
+function  displace = Displacement(large0,small0)
+large0 = 0; small0 = 0;
+
+%Message box with Tare and Read buttons
+    % Displays reading only
+
+% take large and small angles (ZERO button)
+    % store zero angles
+    % small angle (if 0-9) gets converted to degrees
+    
+% take large and small angles again (Read button)
+    % input new angles
+        % total small angle movement gives approximate total displacement
+        % that gives a range the large hand should be in
+        % calculated difference between lage hand angles + 360*n
+    % calculate displacement reading
+    % display displacement reading
+
+
+
+end
